89faf1f0c23edfa51a340da5127b8725bd8d8903
Muthu <muthu@example.com>
2024-04-07 22:22:56 +0530

    second commit

--- MCU_TO_PC/read_from_EEPROM.ino
+++ MCU_TO_PC/read_from_EEPROM.ino
@@ -6,22 +6,14 @@ void setup() {
 
 void loop() {
   if (Serial.available() > 0) {
-    // Read the starting address and length of the string from serial monitor
-    int startAddress = Serial.parseInt();
-    int strLength = Serial.parseInt();
-    
-    // Read each character of the string from EEPROM and assemble it
-    String receivedString = "";
-    for (int i = 0; i < strLength; i++) {
-      char ch = EEPROM.read(startAddress + i);
-      receivedString += ch;
+    int initial_add = Serial.parseInt();
+    int len = Serial.parseInt();
+    String r_str = "";
+    for (int i = 0; i < len; i++) {
+      char ch = EEPROM.read(initial_add + i);
+      r_str=r_str+ch;
     }
-    
-    // Print the received string to serial monitor
-    // Serial.print("Read string from EEPROM: ");
-    Serial.println(receivedString);
-    
-    // Wait for a moment to allow serial data to be received
+    Serial.println(r_str);
     delay(100);
   }
 }
--- PC_TO_MCU/write_to_EEPROM.ino
+++ PC_TO_MCU/write_to_EEPROM.ino
@@ -7,21 +7,13 @@ void setup() {
 void loop() {
   if (Serial.available() > 0) {
     String r_str = Serial.readStringUntil('\n');
-    
-    
     int l = r_str.length();
-    
-    // Write each character of the string to EEPROM
-    int address = 0; // starting address in EEPROM
+    int index = 0; 
     for (int i = 0; i < l; i++) {
-      EEPROM.write(address + i, r_str[i]);
+      EEPROM.write(index + i, r_str[i]);
     }
-    
-    // Print feedback to serial monitor
     Serial.print("Received string: ");
     Serial.println(r_str);
-    
-    // Wait for a moment to allow serial data to be received
     delay(100);
   }
 }
